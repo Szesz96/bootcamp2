ffeb87cbb407bc29a6485d72ca9a3c873c674dd3
Szesz96 <szesza.ka@wp.pl>
2024-06-26 19:11:05 +0200

    added styles

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,15 +1,25 @@
 <template>
      <div>
-         <button @click="pobierzWpisy">refresh</button>
-         siema blog!
-         {{ wpisy }}
-         <input v-model="nowyBlog" type="text">
-         <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-500">Wpisy na bloga:</h2>
+         <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-700 rounded-md text-white p-4">refresh</button>
+         </div>
+         <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class=" drop-shadow-xl bg-stone-300 p-4">
+            <p>{{ wpis }}</p>
+         </div> 
+         </div>
+         <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class=" border-1 border-blue-600 p-4" type="text">
+            <button @click="dodajWpisy" class="bg-blue-700 rounded-md text-white p-4">dodaj</button>
+         </div>
+         
      </div>
  </template>
  
  <script>
  import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
  export default {
      data() {
          return {
@@ -25,5 +35,8 @@
              this.wpisy = await dzien2_backend.odczytaj_wpisy();
          }
      },
+     async mounted(){
+        this.pobierzWpisy()
+     }
  }
  </script>
